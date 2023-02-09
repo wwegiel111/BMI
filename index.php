7b42166aac7b5f744b498385aa3f95ccbfd50360
wwegiel111 <103195873+wwegiel111@users.noreply.github.com>
2023-02-09 10:30:08 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>BMI</title>
+</head>
+
+<?php
+    if(isset($_POST['sex'])){
+        if($_POST['sex'] === 'k'){
+            echo "kobieta";
+        } else {
+            echo "facet";
+        }
+    
+       } else {   
+        echo "nie wybrales plci";
+       }
+    
+
+    if(!empty($_GET['imie'])){
+        echo "<br>Witaj " .$_GET['imie'];
+    } else {
+        echo "<br>Nie wpisales imienia";
+    }
+
+    !empty($_GET['wzrost']) && !empty($_GET['waga']);
+    $waga = $_GET['waga'];
+    $wzrost = $_GET['wzrost'];
+
+
+
+    $bmi = $waga/($wzrost/100*$wzrost/100);
+    echo  "Twoje BMI wynosi $bmi.<br>";
+
+    if ($bmi < "16"){
+        echo "Jesteś wygłodzony.";
+    }
+    else if ($bmi > "16" && $bmi < "17")
+    {
+        echo "Jesteś wychudzony.";
+    }else if ( $bmi > "17.01"  && $bmi < "18.5"){
+        echo "Masz niedowagę.";
+    }else if ($bmi >"18.51"  && $bmi < "25"){
+        echo "Twoja waga jest prawidłowa.";
+    }else if ($bmi >"25.01" && $bmi < "30"){
+        echo "Masz nadwagę.";
+    }else if($bmi > "30.01" && $bmi < "35"){
+        echo "Masz otyłość I stopnia.";
+    }else if($bmi > "35.01" && $bmi < "40"){
+        echo "Masz otyłość II stopnia.";
+    }else{
+        echo "Masz otyłość III stopnia";
+    }
+
+    ?>
+
+
+<body onLoad="preLoad()">
+    <form class="form1">
+        
+    <input placeholder="Podaj imie" type="text" name="imie">
+    <input type="submit" value="wyslij">
+        <div class="gender">
+            <div class="g1">
+            <p>WYBIERZ PŁEĆ:</p>
+            </div>
+            <div class="g1">
+            <input type="radio" id="m" name="gender" value="m" onClick="im('a1');" name="sex" checked="yes">
+            <label for="m">M</label>
+            <input type="radio" id="k" name="gender" value="k" onClick="im('a2');" name="sex">
+            <label for="k">K</label>
+            </div>
+        </div>
+    </form>
+    <div class="imgs">
+        <img id="ruler" src="ruler.png" height="375px">
+        <img id="a">
+    </div>
+    <form class="form1">
+        <div class="height">
+            <label for="h">WZROST</label>
+            <input type="range" min="40" max="250" value="180" id="h" name="wzrost">
+            <input type="number" id="n1">
+            <span id="value1"></span>
+        </div>
+        <div class="weight">
+            <label for="w">WAGA</label>
+            <input type="range" min="1" max="350" value="70" id="w" name="waga">
+            <input type="number" id="n2">
+            <span id="value2"></span>
+        </div>
+        <div class="submit">
+            <input type="submit" value="SPRAWDŹ BMI">
+        </div>
+    </form>
+    <script src="script.js">
+    </script>
+</body>
+</html>
